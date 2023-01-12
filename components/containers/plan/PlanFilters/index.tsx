f1770b3fda13f10ffd2ecc09031bb15ae7366557
kimxfajardo <67940664+kimpfajardo@users.noreply.github.com>
2023-01-13 00:22:43 +0800

    Lint fixes

--- components/containers/plan/PlanFilters/index.tsx
+++ components/containers/plan/PlanFilters/index.tsx
@@ -7,7 +7,7 @@ const PlanFilters = () => {
     <div className="h-auto py-2 pl-8 flex gap-2">
       {
         filters.map((item: any)=> {
-          return <Select {...item} menuCn="left-0" displayLabel={false} />;
+          return <Select {...item} key={item} menuCn="left-0" displayLabel={false} />;
         })
       }
     </div>
